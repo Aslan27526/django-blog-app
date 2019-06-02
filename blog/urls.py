ef9e42d761a1335ca06c24b68d8fa3eac04beba2
Aslan <47611260+Aslan27526@users.noreply.github.com>
2019-06-02 16:22:17 +0400

    Update urls.py

--- blog/urls.py
+++ blog/urls.py
@@ -24,7 +24,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index,name="index"),
     path('about/', views.about,name="about"),
- #   path('detail/<int:id>', views.detail,name="detail"),
     path('articles/',include('article.urls')),
     path('user/',include('user.urls')),
 
